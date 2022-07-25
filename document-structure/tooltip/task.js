60936334940664d85c544e6e69126827de0359e8
Владимир Александрович Райгель <vraygel@mail.ru>
2022-07-25 18:03:41 +0300

    1 задание исправление ошибок

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,31 +1,23 @@
-
 const hasTooltip = document.querySelectorAll('.has-tooltip')
 let tooltipActive = document.getElementsByClassName('tooltip_active')
-
-function defaultFalse() {
-    for (const iterator of hasTooltip) {     
-        return false
-    }
-}
 
 for (const iterator of hasTooltip) {
     iterator.addEventListener('click', func)    
 }
 
 function func(event) {
-
         let leftSearch = event.target.getBoundingClientRect().left
-        let topSearh = event.target.getBoundingClientRect().top + 17
+        let topSearch = event.target.getBoundingClientRect().top + 17
 
-        if(tooltipActive[0]){          
-           tooltipActive[0].remove()
-        } else{
-            event.target.insertAdjacentHTML('afterEnd', `<div class="tooltip tooltip_active" style="left: ${leftSearch}px; top: ${topSearh}px ">${event.target.title}</div>`)   
-        }
-        
+        if(tooltipActive[0]){  
+            if(tooltipActive[0].textContent == event.target.getAttribute('title')){
+                tooltipActive[0].remove()
+            } else{
+                tooltipActive[0].remove()
+                event.target.insertAdjacentHTML('afterEnd', `<div class="tooltip tooltip_active" style="left: ${leftSearch}px; top: ${topSearch}px ">${event.target.title}</div>`)
+                }
+            } else{
+            event.target.insertAdjacentHTML('afterEnd', `<div class="tooltip tooltip_active" style="left: ${leftSearch}px; top: ${topSearch}px ">${event.target.title}</div>`)   
+        } 
         event.preventDefault()
-
-
-
-
-
+}
