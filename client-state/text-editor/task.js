ce3969832592c95eeebc1b8dda8d2113b364d1c2
Владимир Александрович Райгель <vraygel@mail.ru>
2022-07-30 20:51:35 +0300

    1 задание

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+const editor = document.querySelector('#editor');
+const button = document.querySelector('button');
+let editTextGet = localStorage.getItem('text');
+
+if(editTextGet){
+    editor.value = editTextGet;
+}
+
+let editTextSet = ''
+editor.oninput = () => {
+    editTextSet = editor.value
+    log(editTextSet)
+    localStorage.setItem('text', editTextSet)
+}
+
+button.onclick = () => {
+    editor.value = null
+}
