cd71f0b095a0b99dd7f62ffd00528d77f47b1016
Владимир Александрович Райгель <vraygel@mail.ru>
2022-07-30 16:22:55 +0300

    1 задание

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,56 +1,25 @@
-const loader = document.querySelector('.loader')
-const itemDiv = document.querySelector('.item')
-
-let log = console.log
-//log(loader)
- 
-
-
-
+const loader = document.querySelector('.loader');
+const itemDiv = document.querySelector('.item');
 let xhr = new XMLHttpRequest();
-// экземпляр объекта XMLHttpRequest
-//console.log(xhr);
 
-xhr.open('get', 'https://netology-slow-rest.herokuapp.com')
+xhr.open('get', 'https://netology-slow-rest.herokuapp.com');
 xhr.send();
-
 xhr.onreadystatechange = function () {
     if(xhr.readyState === 4) {
-    loader.classList.remove('loader_active')
-    //console.log(xhr.responseText)
-    
-    let value = JSON.parse(xhr.responseText);
-    //log(Object.entries(value.response.Valute)[0])
-    Object.entries(value.response.Valute).forEach((item, index) =>{
-        log(Object.entries(item)[1][1])
-        itemDiv.insertAdjacentHTML('afterEnd', `<div class="item">
-        <div class="item__code">
-        ${Object.entries(item)[1][1].CharCode} (${Object.entries(item)[1][1].Name})
-        </div>
-        <div class="item__value">
-        = ${Object.entries(item)[1][1].Value}
-        </div>
-        <div class="item__currency">
-        руб.
-        </div>
-        </div>`)
-    })
-
-   
-
+        loader.classList.remove('loader_active');
+        let value = JSON.parse(xhr.responseText);
+        Object.entries(value.response.Valute).forEach((item) =>{
+            itemDiv.insertAdjacentHTML('afterEnd', `<div class="item">
+            <div class="item__code">
+            ${Object.entries(item)[1][1].CharCode}
+            </div>
+            <div class="item__value">
+            ${Object.entries(item)[1][1].Value}
+            </div>
+            <div class="item__currency">
+            руб.
+            </div>
+            </div>`)
+        })
     }
 }
-    
-
-
-
-
-item.insertAdjacentHTML('afterEnd', `<div class="item__code">
-USD
-</div>
-<div class="item__value">
-32
-</div>
-<div class="item__currency">
-руб.
-</div>`)
